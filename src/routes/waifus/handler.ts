56d409d7f6eb4adfe8e9997950c1faed73ac30d4
Phineas <phin@phineas.io>
2021-03-25 00:29:19 +0000

    format attributes for opensea

--- src/routes/waifus/handler.ts
+++ src/routes/waifus/handler.ts
@@ -15,6 +15,13 @@ export const getWaifuByTokenId = async (
     revealedTokenIndex
   );
 
+  const openSeaFormattedAttributes = Object.entries(attributes).map(([trait_type, value]) => {
+    return {
+      trait_type,
+      value
+    }
+  })
+
   const ipfsUrl = `${Config.IPFS_PREFIX}/${revealedTokenIndex}.png`;
   const detailUrl = `https://waifusion.sexy/detail/${revealedTokenIndex}`;
 
@@ -24,6 +31,6 @@ export const getWaifuByTokenId = async (
       "ATTENTION: Waifu names can change at any time. Immediately before purchasing a Waifu, enter the Waifu's token ID into the tokenNameByIndex function on a site like Etherscan to verify that the blockchain indicates that the Waifu you're purchasing has the name you expect. Waifu #INDEX",
     image: ipfsUrl,
     external_url: detailUrl,
-    attributes,
+    attributes: openSeaFormattedAttributes,
   });
 };
