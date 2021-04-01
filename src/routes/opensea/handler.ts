c19dbcdf5e620e14f3aa989c4797e0924ff60758
Phineas <phin@phineas.io>
2021-04-01 02:06:27 +0100

    Update OpenSea description

--- src/routes/opensea/handler.ts
+++ src/routes/opensea/handler.ts
@@ -28,7 +28,7 @@ export const getWaifuByTokenId = async (
   reply.send({
     name,
     description:
-      "ATTENTION: Waifu names can change at any time. Immediately before purchasing a Waifu, enter the Waifu's token ID into the tokenNameByIndex function on a site like Etherscan to verify that the blockchain indicates that the Waifu you're purchasing has the name you expect. Waifu #INDEX",
+      `Waifu #${req.params.tokenId} | You can claim WET and change your Waifu's name on [waifusion.sexy](https://waifusion.sexy).\n\nWarning: Waifu names can change at any time. Immediately before purchasing a Waifu, enter the Waifu's token ID into the tokenNameByIndex function on a site like Etherscan to verify that the blockchain indicates that the Waifu you're purchasing has the name you expect.`,
     image: ipfsUrl,
     external_url: detailUrl,
     attributes: openSeaFormattedAttributes,
