3c34d91fc90736225e423661c2afb8c7652addac
Phineas <phin@phineas.io>
2021-04-01 02:48:56 +0100

    Update OpenSea description and external link

--- src/routes/opensea/handler.ts
+++ src/routes/opensea/handler.ts
@@ -23,12 +23,12 @@ export const getWaifuByTokenId = async (
   })
 
   const ipfsUrl = `${Config.IPFS_PREFIX}/${revealedTokenIndex}.png`;
-  const detailUrl = `https://waifusion.sexy/detail/${revealedTokenIndex}`;
+  const detailUrl = `https://waifusion.sexy/app/detail/${revealedTokenIndex}`;
 
   reply.send({
     name,
     description:
-      `Waifu #${req.params.waifuId} | You can claim WET and change your Waifu's name on [waifusion.sexy](https://waifusion.sexy).\n\nWarning: Waifu names can change at any time. Immediately before purchasing a Waifu, enter the Waifu's token ID into the tokenNameByIndex function on a site like Etherscan to verify that the blockchain indicates that the Waifu you're purchasing has the name you expect.`,
+      `Waifu #${req.params.waifuId}\n\nYou can claim WET and change your Waifu's name on [waifusion.sexy](https://waifusion.sexy).\n\nWaifusion is a digital Waifu collection. There are 16,384 guaranteed-unique Waifusion NFTs. They’re just like you; a beautiful work of art, but 2-D and therefore, superior, Anon-kun. Each Waifu is wholly unique and yours forever... unless you sell them... Baka.\n\n**Warning**: Waifu names can change at any time. Immediately before purchasing a Waifu, enter the Waifu's token ID into the tokenNameByIndex function on a site like Etherscan to verify that the blockchain indicates that the Waifu you're purchasing has the name you expect.`,
     image: ipfsUrl,
     external_url: detailUrl,
     attributes: openSeaFormattedAttributes,
