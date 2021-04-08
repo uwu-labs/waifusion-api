3433427c94e09552149f8b68974d7fa5d3c93397
Chase Manning <me@chasemanning.co.nz>
2021-04-08 19:56:40 +1200

    :rocket: add get bsc waifu endpoint

--- src/routes/waifus/index.ts
+++ src/routes/waifus/index.ts
@@ -1,9 +1,14 @@
 import { IWaifusionInstance } from "../../types/Fastify";
-import { filterWaifus, getWaifuByTokenId } from "./handler";
+import {
+  filterWaifus,
+  getBscWaifuByTokenId,
+  getWaifuByTokenId,
+} from "./handler";
 
 export default (fastify: IWaifusionInstance, _opts: any, next: any) => {
   fastify.get("/:waifuId", getWaifuByTokenId);
+  fastify.get("/bsc/:waifuId", getBscWaifuByTokenId);
   fastify.get("/filter", filterWaifus);
 
   next();
-}
+};
